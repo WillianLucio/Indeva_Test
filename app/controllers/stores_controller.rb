28e2ca4690a0f15b6db43f37b55395da42c2eaa0
Willian Lucio <willian.l.lucio@gmail.com>
2017-11-27 21:03:13 -0200

    create model and controller for store

---
+++ app/controllers/stores_controller.rb
@@ -0,0 +1,16 @@
+class StoresController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'stores/show'
+
+  get 'stores/index'
+
+  get 'stores/create'
+
+  get 'stores/update'
+
+  get 'stores/destroy'
+
   devise_for :users, :controllers => { registrations: 'registrations' }
 end
