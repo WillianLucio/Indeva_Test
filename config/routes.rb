f258533016147db3bdf6c8cb9a9c0d10e98ac895
Willian Lucio <willian.l.lucio@gmail.com>
2017-11-29 19:20:09 -0200

    adjust store routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  resources :stores, only: [:index, :show, :create, :update, :destroy]
+  resources :stores
   devise_for :users, :controllers => { registrations: 'registrations' }
 end
