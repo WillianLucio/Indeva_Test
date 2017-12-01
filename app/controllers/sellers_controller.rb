d84468a8f70d272685dda925953048573673721d
Willian Lucio <willian.l.lucio@gmail.com>
2017-12-01 15:26:38 -0200

    created model seller and controller Sellers

---
+++ app/controllers/sellers_controller.rb
@@ -0,0 +1,25 @@
+class SellersController < ApplicationController
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def index
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def raffle
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  get 'sellers/show'
+
+  get 'sellers/new'
+
+  get 'sellers/edit'
+
+  get 'sellers/index'
+
+  get 'sellers/create'
+
+  get 'sellers/update'
+
+  get 'sellers/destroy'
+
+  get 'sellers/raffle'
+
   root to: 'home#index'
   resources :stores
   devise_for :users, :controllers => { registrations: 'registrations' }
