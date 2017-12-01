c205eef5a3ef512d618a4fc4756d914e921f817c
Willian Lucio <willian.l.lucio@gmail.com>
2017-12-01 16:03:56 -0200

    created actions in sellers_controller

--- app/controllers/sellers_controller.rb
+++ app/controllers/sellers_controller.rb
@@ -1,25 +1,55 @@
 class SellersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_seller, only: [:edit, :show, :update, :destroy]
+
   def show
   end
 
   def new
+    @seller = Seller.new
   end
 
   def edit
   end
 
-  def index
-  end
-
   def create
+    @seller = Seller.new(seller_params)
+
+    respond_to do |format|
+      if @seller.save
+        format.html { redirect_to "/sellers" }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
+    respond_to do |format|
+      if @seller.update(seller_params)
+        format.html { redirect_to "/sellers" }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    @seller.destroy
+
+    respond_to do |format|
+      format.html { redirect_to "/sellers" }
+    end
   end
 
-  def raffle
+  private
+
+  def set_seller
+    @seller = Seller.find(params[:id])
   end
+
+  def seller_params
+    params.require(:seller).permit(:name, :store_id)
+  end
+
 end
